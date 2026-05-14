4e594af3757ac39ad6adcf05b9ce155b5fed0aff
Valentina Schüller <valentina.schueller@gmail.com>
2026-05-14 15:42:10 +0200

    Add wander console

---
+++ wander/wander.js
@@ -0,0 +1,23 @@
+const wander = {
+  // Other Wander consoles that visitors can reach from my console.
+  consoles: [
+    'https://susam.net/wander/',
+  ],
+
+  // My favourite websites and pages I recommend to the Wander community.
+  pages: [
+    'https://jvns.ca',
+    'https://annaladd.lol',
+    'https://www.marginalia.nu',
+    'https://bavaria-ipsum.de',
+    'https://hballington12.github.io/oignon/',
+    'https://internetphonebook.net',
+    'https://www.noidea.dog/glue',
+  ],
+
+  // Websites and consoles to ignore.  My console will never fetch
+  // consoles or web pages whose URLs match the following patterns.
+  ignore: [
+    // Fake examples, please replace with actual URLs.
+  ],
+}
